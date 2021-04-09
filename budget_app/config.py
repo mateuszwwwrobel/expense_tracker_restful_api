24955cfbdef4922b588e07eb7ed9a101cd3a4443
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-04-09 15:52:12 +0100

    Add config.py - configuration for SQLAlchemy

---
+++ budget_app/config.py
@@ -0,0 +1,7 @@
+
+class Config:
+    # General
+    DEBUG = True
+    # SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = 'sqlite://///home/matt/Programming/Apka_budget/budget_app/database.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
