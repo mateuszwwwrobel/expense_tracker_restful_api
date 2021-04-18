6579a4519a0b423f04fd6b23899fb6e9b71b06e3
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-04-18 09:24:27 +0100

    Rename main directory

--- api/config.py
+++ api/config.py
@@ -3,7 +3,7 @@ class Config:
     # General
     DEBUG = True
     # SQLAlchemy
-    SQLALCHEMY_DATABASE_URI = 'sqlite://///home/matt/Programming/Apka_budget/budget_app/database.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite://///home/matt/Programming/Expense_Tracker/backend/api/database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # JWT TOKEN
     SECRET_KEY = 'bna]][yu8dga[.]]s6d87`gd79asd[ddasd;[;[.q211`109j'
