84908df35ff2d38800029f3558f2ec151d436b0f
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-04-26 19:35:38 +0100

    modify config

--- api/config.py
+++ api/config.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 
 class Config:
     # General
@@ -6,5 +8,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite://///home/matt/Programming/Expense_Tracker/backend/api/database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # JWT TOKEN
-    SECRET_KEY = 'bna]][yu8dga[.]]s6d87`gd79asd[ddasd;[;[.q211`109j'
+    SECRET_KEY = 'super_secret_key'
     JWT_ERROR_MESSAGE_KEY = 'message'
